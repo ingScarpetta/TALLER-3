d42e92349bf32147117de299697f972630720ef2
Estudiantes <Estudiantes@labing50508.udistrital.edu.co>
2017-10-14 16:39:58 -0500

    creacion de servicio web que importa las librerias que usara el aplicativo web

---
+++ WEBSERVICE/src/java/org/me/lecturas/lecturas.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.me.lecturas;
+
+import javax.jws.WebService;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Estudiantes
+ */
+@WebService(serviceName = "lecturas")
+@Stateless()
+public class lecturas {
+
+    /**
+     * This is a sample web service operation
+     */
+    @WebMethod(operationName = "hello")
+    public String hello(@WebParam(name = "name") String txt) {
+        return "Hello " + txt + " !";
+    }
+}
